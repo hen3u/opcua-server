8a7d5c81b9640ca73a4aa69a42619e5a1269d7e8
hen3u <loic.hen@telecomnancy.net>
2024-12-22 23:02:20 +0100

    main: move server to asynchronous mode

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,89 @@
 #[macro_use]
 extern crate log;
 
-use std::path::PathBuf;
 use std::sync::Arc;
 
-use opcua::server::{
-    address_space::method::MethodBuilder, callbacks, prelude::*, session::SessionManager,
+use log::warn;
+use opcua::server::address_space::{Variable, EventNotifier, MethodBuilder, ObjectBuilder};
+use opcua::server::node_manager::memory::{
+    simple_node_manager, InMemoryNodeManager, NamespaceMetadata, SimpleNodeManager,
+    SimpleNodeManagerImpl,
 };
-
-use opcua::sync::Mutex;
-use opcua::sync::RwLock;
+use opcua::server::ServerBuilder;
+use opcua::types::{DataValue, NodeId, ObjectId};
 
 use std::process::Command;
 
-mod alarms;
-
-struct TriggerEvent;
-
-impl callbacks::Method for TriggerEvent {
-    fn call(
-        &mut self,
-        _session_id: &NodeId,
-        _session_map: Arc<RwLock<SessionManager>>,
-        _request: &CallMethodRequest,
-    ) -> Result<CallMethodResult, StatusCode> {
-        debug!("TriggerEvent method called");
-        Ok(CallMethodResult {
-            status_code: StatusCode::Good,
-            input_argument_results: None,
-            input_argument_diagnostic_infos: None,
-            output_arguments: None,
-        })
-    }
-}
-
-fn main() {
+#[tokio::main]
+async fn main() {
     log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
 
-    let mut server = Server::new(ServerConfig::load(&PathBuf::from("./server.conf")).unwrap());
+    // Create an OPC UA server with sample configuration and default node set
 
-    let ns = {
-        let address_space = server.address_space();
-        let mut address_space = address_space.write();
-        address_space.register_namespace("urn:rust-server").unwrap()
-    };
+    let (server, handle) = ServerBuilder::new()
+        .with_config_from("./server.conf")
+        .with_node_manager(simple_node_manager(
+            NamespaceMetadata {
+                namespace_uri: "urn:DIYServer".to_owned(),
+                ..Default::default()
+            },
+            "demo",
+        ))
+        .build()
+        .unwrap();
 
-    // Add some alarms
-    alarms::add_alarms(&mut server, ns);
+    let node_manager = handle
+        .node_managers()
+        .get_of_type::<SimpleNodeManager>()
+        .unwrap();
+    let ns = handle.get_namespace_index("urn:DIYServer").unwrap();
 
+    add_sensor_variables(ns, node_manager);
+
+    // If you don't register a ctrl-c handler, the server will close without
+    // informing clients.
+    let handle_c = handle.clone();
+    tokio::spawn(async move {
+        if let Err(e) = tokio::signal::ctrl_c().await {
+            warn!("Failed to register CTRL-C handler: {e}");
+            return;
+        }
+        handle_c.cancel();
+    });
+
+    // Run the server. This does not ordinarily exit so you must Ctrl+C to terminate
+    server.run().await.unwrap();
+}
+
+fn add_sensor_variables(ns: u16, manager: Arc<InMemoryNodeManager<SimpleNodeManagerImpl>>) {
+    // Create an OPC UA variable to hold the CPU temperature data
+    let temp_node = NodeId::new(ns, "cpu_temperature");
+
+    // Raspberry Pi 3 specific
+    let cpu_thermal_virtual = NodeId::new(ns, "cpu_thermal-virtual-0");
+    let piface_led7_node = NodeId::new(ns, "piface_led7");
+    let piface_switch3_node = NodeId::new(ns, "piface_switch3");
+    let object_id = NodeId::new(ns, "Methods");
+
+    let address_space = manager.address_space();
     {
-        let address_space = server.address_space();
         let mut address_space = address_space.write();
 
-        let object_id = NodeId::new(ns, "Methods");
         ObjectBuilder::new(&object_id, "Methods", "Methods")
             .event_notifier(EventNotifier::SUBSCRIBE_TO_EVENTS)
             .organized_by(ObjectId::ObjectsFolder)
-            .insert(&mut address_space);
+            .insert(&mut *address_space);
 
-        let fn_node_id = NodeId::new(ns, "TriggerEvent");
-        MethodBuilder::new(&fn_node_id, "TriggerEvent", "TriggerEvent")
+        let trigger_node_id = NodeId::new(ns, "TriggerEvent");
+        MethodBuilder::new(&trigger_node_id, "TriggerEvent", "TriggerEvent")
             .component_of(object_id.clone())
-            .callback(Box::new(TriggerEvent))
-            .insert(&mut address_space);
-    }
-
-    // Create an OPC UA variable to hold the CPU temperature data
-    let temp_node = NodeId::new(ns, "cpu_temperature");
-    {
-        let address_space = server.address_space();
-        let mut address_space = address_space.write();
+            .executable(true)
+            .user_executable(true)
+            .insert(&mut *address_space);
+        manager.inner().add_method_callback(trigger_node_id, |_| {
+            debug!("TriggerEvent method called");
+            Ok(Vec::new())
+        });
 
         let _ = address_space.add_variables(
             vec![Variable::new(
@@ -80,12 +94,6 @@ fn main() {
             )],
             &NodeId::objects_folder_id(),
         );
-    }
-
-    let cpu_thermal_virtual = NodeId::new(ns, "cpu_thermal-virtual-0");
-    {
-        let address_space = server.address_space();
-        let mut address_space = address_space.write();
 
         let _ = address_space.add_variables(
             vec![Variable::new(
@@ -96,18 +104,14 @@ fn main() {
             )],
             &NodeId::objects_folder_id(),
         );
-    }
 
-    {
-        let piface_led7_node = NodeId::new(ns, "piface_led7");
-        let piface_switch3_node = NodeId::new(ns, "piface_switch3");
-
-        let address_space = server.address_space();
-        let mut address_space = address_space.write();
-
-        let piface_nodeid = address_space
-            .add_folder("piface", "piface", &NodeId::objects_folder_id())
-            .unwrap();
+        let piface_folder_id = NodeId::new(ns, "piface");
+        address_space.add_folder(
+            &piface_folder_id,
+            "piface",
+            "piface",
+            &NodeId::objects_folder_id(),
+        );
 
         let _ = address_space.add_variables(
             vec![
@@ -119,71 +123,53 @@ fn main() {
                 ),
                 Variable::new(&piface_led7_node, "piface_led7", "piface_led7", false),
             ],
-            &piface_nodeid,
+            &piface_folder_id,
         );
     }
 
-    // Update the OPC UA variable with the CPU temperature data
     {
-        let address_space = server.address_space();
-        let mut address_space = address_space.write();
+        manager
+            .inner()
+            .add_read_callback(temp_node.clone(), move |_, _, _| {
+                // Get the CPU temperature using lm-sensors
+                let output = match Command::new("sensors").output() {
+                    Ok(output) => output,
+                    Err(err) => {
+                        log::error!("Failed to execute 'sensors' command: {}", err);
+                        return Ok(DataValue::new_now(0.0));
+                    }
+                };
+                let temp_str = String::from_utf8_lossy(&output.stdout);
+                let temp = temp_str
+                    .lines()
+                    .find(|line| line.contains("Core"))
+                    .and_then(|line| line.split_whitespace().nth(2))
+                    .and_then(|temp_str| temp_str.strip_suffix("°C"))
+                    .and_then(|temp_str| temp_str.parse::<f32>().ok())
+                    .unwrap_or(0.0);
+                Ok(DataValue::new_now(temp))
+            });
 
-        // Update the temperature variable
-        if let Some(ref mut v) = address_space.find_variable_mut(temp_node.clone()) {
-            let getter = AttrFnGetter::new(
-                move |_, _, _, _, _, _| -> Result<Option<DataValue>, StatusCode> {
-                    // Get the CPU temperature using lm-sensors
-                    let output = match Command::new("sensors").output() {
-                        Ok(output) => output,
-                        Err(err) => {
-                            log::error!("Failed to execute 'sensors' command: {}", err);
-                            return Ok(Some(DataValue::new_now(0.0)));
-                        }
-                    };
-                    let temp_str = String::from_utf8_lossy(&output.stdout);
-                    let temp = temp_str
-                        .lines()
-                        .find(|line| line.contains("Core"))
-                        .and_then(|line| line.split_whitespace().nth(2))
-                        .and_then(|temp_str| temp_str.strip_suffix("°C"))
-                        .and_then(|temp_str| temp_str.parse::<f32>().ok())
-                        .unwrap_or(0.0);
-                    Ok(Some(DataValue::new_now(temp)))
-                },
-            );
-            v.set_value_getter(Arc::new(Mutex::new(getter)));
-        } else {
-            log::warn!("Temperature variable not found in the address space");
-        }
-
-        // Update the CPU thermal virtual variable
-        if let Some(ref mut v) = address_space.find_variable_mut(cpu_thermal_virtual.clone()) {
-            let getter = AttrFnGetter::new(
-                move |_, _, _, _, _, _| -> Result<Option<DataValue>, StatusCode> {
-                    // Get the CPU temperature using lm-sensors
-                    let output = match Command::new("sensors").output() {
-                        Ok(output) => output,
-                        Err(err) => {
-                            log::error!("Failed to execute 'sensors' command: {}", err);
-                            return Ok(Some(DataValue::new_now(0.0)));
-                        }
-                    };
-                    let temp_str = String::from_utf8_lossy(&output.stdout);
-                    let temp = temp_str
-                        .lines()
-                        .find(|line| line.contains("temp1"))
-                        .and_then(|line| line.split_whitespace().nth(1))
-                        .and_then(|temp_str| temp_str.strip_suffix("°C"))
-                        .and_then(|temp_str| temp_str.parse::<f32>().ok())
-                        .unwrap_or(0.0);
-                    Ok(Some(DataValue::new_now(temp)))
-                },
-            );
-            v.set_value_getter(Arc::new(Mutex::new(getter)));
-        } else {
-            log::warn!("CPU thermal virtual variable not found in the address space");
-        }
+        manager
+            .inner()
+            .add_read_callback(cpu_thermal_virtual.clone(), move |_, _, _| {
+                // Get the CPU temperature using lm-sensors
+                let output = match Command::new("sensors").output() {
+                    Ok(output) => output,
+                    Err(err) => {
+                        log::error!("Failed to execute 'sensors' command: {}", err);
+                        return Ok(DataValue::new_now(0.0));
+                    }
+                };
+                let temp_str = String::from_utf8_lossy(&output.stdout);
+                let temp = temp_str
+                    .lines()
+                    .find(|line| line.contains("temp1"))
+                    .and_then(|line| line.split_whitespace().nth(1))
+                    .and_then(|temp_str| temp_str.strip_suffix("°C"))
+                    .and_then(|temp_str| temp_str.parse::<f32>().ok())
+                    .unwrap_or(0.0);
+                Ok(DataValue::new_now(temp))
+            });
     }
-
-    server.run();
 }
